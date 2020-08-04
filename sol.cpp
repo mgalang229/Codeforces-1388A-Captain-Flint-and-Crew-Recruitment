a071e7f509deb4cab05e12e4032f27d616b493ae
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2020-08-04 09:54:51 +0800

    Update sol.cpp

--- sol.cpp
+++ sol.cpp
@@ -2,25 +2,32 @@
 
 using namespace std;
 
+void test_case() {
+	int n;
+	cin >> n;
+	if(n <= 30) {
+		cout << "NO";
+	}
+	else {
+		cout << "YES" << "\n";
+		if(n == 36 || n == 40 || n == 44) {
+			cout << 6 << " " << 10 << " " << 15 << " " << n - 31;
+		}
+		else {
+			cout << 6 << " " << 10 << " " << 14 << " " << n - 30;
+		}
+	}
+	cout << "\n";
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
-	int tt;
-	cin >> tt;
-	for (int qq = 1; qq <= tt; qq++) {
-		int n;
-		cin >> n;
-		if (n <= 30) {
-			cout << "NO";
-		} else {
-			cout << "YES" << '\n';
-			if (n == 36 || n == 40 || n == 44) {
-				cout << 6 << ' ' << 10 << ' ' << 15 << ' ' << n - 31;
-			} else {
-				cout << 6 << ' ' << 10 << ' ' << 14 << ' ' << n - 30;
-			}
-		}
-		cout << '\n';
+	int T;
+	cin >> T;
+	for(int nr = 1; nr <= T; ++nr) {
+		test_case();
 	}
+	//test_case();
 	return 0;
 }
